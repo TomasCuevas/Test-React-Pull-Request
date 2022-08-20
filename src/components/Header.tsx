b4fbd088f480d0c5c36c5a89b75fe35046c9c9a8
Tomas Cuevas <memo_mister398@hotmail.com>
2022-08-20 14:39:29 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="flex h-[80px] w-full items-center bg-orange-500 px-[5%]">
+      <h1 className="text-4xl font-black uppercase text-white">Header</h1>
+    </header>
+  );
+};
