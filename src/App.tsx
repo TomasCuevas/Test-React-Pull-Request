ede698b61153e2082de2aec61fc0dc6e0b03f70f
Tomas Cuevas <memo_mister398@hotmail.com>
2022-08-20 17:38:50 -0300

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import { Header } from "./components/Header";
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 export const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="flex h-[120px] w-full items-center justify-center bg-green-900">
+      <h1 className="text-4xl font-black uppercase text-white">Footer</h1>
+    </footer>
+  );
+};
